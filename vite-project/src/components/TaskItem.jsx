fb40c3aac8fa15da9a90ddfda09e0de830b12cfe
DarianUnU <baldelomardarian@gmail.com>
2025-05-01 21:40:39 -0300

    Agrego boton Realizada y hago pequeños cambios de css

--- vite-project/src/components/TaskItem.jsx
+++ vite-project/src/components/TaskItem.jsx
@@ -1,18 +1,20 @@
 function TaskItem({ task, onToggle, onDelete }) {
-    return (
-      <li>
-        <input 
-          type="checkbox" 
-          checked={task.completed} 
-          onChange={() => onToggle(task.id)} 
-        />
-        <span style={{ textDecoration: task.completed ? 'line-through' : 'none' }}>
-          {task.text}
-        </span>
-        <button onClick={() => onDelete(task.id)}>Eliminar</button>
-      </li>
-    );
-  }
-  
-  export default TaskItem;
+  return (
+    <li>
+      <span style={{ textDecoration: task.completed ? 'line-through' : 'none', marginRight: '1rem' }}>
+        {task.text}
+      </span>
+      
+      <button onClick={() => onToggle(task.id)}>
+        Realizada
+      </button>
+
+      <button onClick={() => onDelete(task.id)} style={{ marginLeft: '0.5rem' }}>
+        Eliminar
+      </button>
+    </li>
+  );
+}
+
+export default TaskItem;
   
