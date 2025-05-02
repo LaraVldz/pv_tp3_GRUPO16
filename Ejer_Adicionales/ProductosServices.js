ee417af73e9eaf597509216f383867483456786f
Ignacio Jes <ignaciojes@gmail.com>
2025-05-02 01:43:49 -0300

    agrego punto 3 y 4

--- Ejer_Adicionales/ProductosServices.js
+++ Ejer_Adicionales/ProductosServices.js
@@ -14,8 +14,28 @@ function mostrarProductos() {
 };
 
 function filtrarProductos() {
+    console.log("2. ");
     const filtrados = Productos.filter(producto => producto.precio > 20);
     filtrados.forEach(producto => {
       console.log(`Producto: ${producto.descripcion} - Precio: $${producto.precio}`);
+    });
+}
+function agregarIVA() {
+  console.log("3. ");
+  const conIVA = Productos.map(p => ({
+    descripcion: p.descripcion,
+    precioConIVA: (p.precio * 1.21).toFixed(2)
+  }));
+  conIVA.forEach(p => {
+    console.log(`Producto: ${p.descripcion} - Precio con IVA: $${p.precioConIVA}`);
+  });
+}
+
+function ordenarProductos() {
+    console.log("4. ");
+    Productos.sort((a, b) => a.precio - b.precio);
+    console.log("Productos ordenados:");
+    Productos.forEach(p => {
+      console.log(`Producto: ${p.descripcion} - Precio: $${p.precio}`);
     });
 }
